76f8a07118fd154014f5ecd8380a8989fcaa772b
Michael Shoup <michael@12southmusic.com>
2014-09-17 13:36:31 -0500

    Delete deploy.php

--- deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $LOCAL_REPO = "/var/www/vhosts/12southdev.com/ -- site -- /wp-content/themes/ -- repo -- ";
-
-  echo shell_exec("cd $LOCAL_REPO");
-  echo shell_exec("git pull origin master");
-?>
-
