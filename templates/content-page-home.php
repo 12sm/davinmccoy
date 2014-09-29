e93ce657e94c95e39889a19d8b6d18ead3b8fcca
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-29 16:20:55 -0500

    home page beginnings

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,144 +1,71 @@
-<section id="home">
-  <div class='row'>
-    <div class='col-sm-12'>
-      <img src='/wp-content/themes/splash/assets/img/logo.png' class="img-responsive logo">
-      <div class="mail-form">
-        <!-- Begin MailChimp Signup Form -->
-        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-          #mc_embed_signup {
-            background: #fff;
-            clear: left;
-            font: 14px Helvetica, Arial, sans-serif;
-          }
-          /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-             We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-        </style>
-        <div id="mc_embed_signup">
-          <form action="//davinmccoy.us8.list-manage.com/subscribe/post?u=4d33b75ac40b7c4a1aa594342&amp;id=7fb168a173"
-                method="post"
-                id="mc-embedded-subscribe-form"
-                name="mc-embedded-subscribe-form"
-                class="validate"
-                target="_blank"
-                novalidate>
-            <div class="mc-field-group">
-              <input type="email" value="" name="EMAIL" class="required email" placeholder='Enter E-Mail Here' id="mce-EMAIL">
-            </div>
-            <div id="mce-responses" class="clear">
-              <div class="response" id="mce-error-response" style="display:none"></div>
-              <div class="response" id="mce-success-response" style="display:none"></div>
-            </div>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;">
-              <input type="text" name="b_4d33b75ac40b7c4a1aa594342_7fb168a173" tabindex="-1" value="">
-            </div>
-            <div class="clear">
-              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-            </div>
-          </form>
-        </div>
-        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-        <script type='text/javascript'>
-          (function($) {
-            window.fnames = new Array();
-            window.ftypes = new Array();
-            fnames[0] = 'EMAIL';
-            ftypes[0] = 'email';
-            fnames[1] = 'FNAME';
-            ftypes[1] = 'text';
-            fnames[2] = 'LNAME';
-            ftypes[2] = 'text';
-            fnames[3] = 'MMERGE3';
-            ftypes[3] = 'text';
-            fnames[4] = 'MMERGE4';
-            ftypes[4] = 'text';
-            fnames[5] = 'MMERGE5';
-            ftypes[5] = 'text';
-          }(jQuery));
-          var $mcj = jQuery.noConflict(true);
-        </script>
-        <!--End mc_embed_signup-->
-      </div>
-      <div class="row">
-        <ul class="social-shots">
-          <!--Drink Up-->
-          <li class="outer left insta col-xs-6 col-sm-2 col-sm-offset-1">
-            <a href="http://instagram.com/davin_mccoy_band" target="_blank">
-              <span class='hover-shot'>
-                <div class="icon-box">
-                  <i class='fa fa-instagram'></i>
-                </div>
-                <div class="shot-mask">
-                  <div class="shot-container"></div>
-                </div>
-              </span>
-            </a>
-          </li>
-          <li class="inner left iTunes col-xs-6 col-sm-2">
-            <a href="https://itunes.apple.com/us/artist/davin-mccoy/id348323607" target="_blank">
-              <span class='hover-shot'>
-                <div class="icon-box">
-                  <i class='fa fa-apple'></i>
-                </div>
-                <div class="shot-mask">
-                  <div class="shot-container"></div>
-                </div>
-              </span>
-            </a>
-          </li>
-          <li class="center facebook col-xs-6 col-sm-2">
-            <a href="https://www.facebook.com/DavinMcCoyMusic" target="_blank">
-              <span class='hover-shot'>
-                <div class="icon-box">
-                  <i class='fa fa-facebook'></i>
-                </div>
-                <div class="shot-mask">
-                  <div class="shot-container"></div>
-                </div>
-              </span>
-            </a>
-          </li>
-          <li class="inner right twitter col-xs-6 col-sm-2">
-            <a href="https://twitter.com/DavinMcCoyBand" target="_blank">
-              <span class='hover-shot'>
-                <div class="icon-box">
-                  <i class='fa fa-twitter'></i>
-                </div>
-                <div class="shot-mask">
-                  <div class="shot-container"></div>
-                </div>
-              </span>
-            </a>
-          </li>
-          <li class="outer right youtube col-xs-6 col-sm-2">
-            <a href="https://www.youtube.com/user/davinmccoy" target="_blank">
-              <span class='hover-shot'>
-                <div class="icon-box">
-                  <i class="fa fa-youtube"></i>
-                </div>
-                <div class="shot-mask">
-                  <div class="shot-container"></div>
-                </div>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
+<div id="stars">
+</div>
+
+<div id="images">
+</div>
+
+<div class="homecontent">
+  <article id="news">
+	</article>
+	<article id="tour">
+	</article>
+	<article id="videos">
+	</article>
+</div>
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+  <head>
+    <title>Parallax Scrolling Example</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/parallax.js"></script>
+  </head>
+  <body>
+		<nav id="main-nav">
+			<ul>
+				<li><a class="earth" href="#earth">Outer Space</a></li>
+				<li><a class="moon" href="#moon">Moon</a></li>
+				<li><a class="rocket" href="#rocket">Rocket</a></li>
+				<li><a class="saturn" href="#saturn">Planets</a></li>
+			</ul>
+		</nav>
+    <div id="earth"><img id="img-earth" src="img/earth.png" alt="Earth"></div>
+    <div id="stars"></div>
+    <div id="images">
+      <img id="img-moon" src="img/moon.png" alt="Moon">
+      <img id="img-rocket" src="img/rocket.png" alt="Rocket">
+      <img id="img-saturn" src="img/saturn.png" alt="Saturn">
     </div>
-  </div>
-</section>
-
-
-
-
-<section id="about">
-</section>
-<section id="shows">
-</section>
-<section id="video">
-</section>
-<section id="photo">
-</section>
-<section id="contact">
-</section>
+    <div id="content">
+      <article id="space">
+        <h2>Welcome to Outer Space</h2>
+        <p>The Universe is all of spacetime and everything that exists therein, including all planets, stars, galaxies, the contents of intergalactic space, the smallest subatomic particles, and all matter and energy. Similar terms include the cosmos, the world, reality, and nature.</p>
+        <p>The observable universe is about 46 billion light years in radius.[7] Scientific observation of the Universe has led to inferences of its earlier stages. These observations suggest that the Universe has been governed by the same physical laws and constants throughout most of its extent and history. The Big Bang theory is the prevailing cosmological model that describes the early development of the Universe, which is calculated to have begun 13.798 ± 0.037 billion years ago. Observations of supernovae have shown that the Universe is expanding at an accelerating rate.</p>
+        <p>There are many competing theories about the ultimate fate of the universe. Physicists remain unsure about what, if anything, preceded the Big Bang. Many refuse to speculate, doubting that any information from any such prior state could ever be accessible. There are various multiverse hypotheses, in which some physicists have suggested that the Universe might be one among many, or even an infinite number, of universes that likewise exist.</p>
+      </article>
+      <article id="moon">
+        <h2>Fly Me to the Moon</h2>
+        <p>The Moon (Latin: Luna) is the Earth's only natural satellite. Although not the largest natural satellite in the Solar System, it is, among the satellites of major planets, the largest relative to the size of the object it orbits (its primary) and, after Jupiter's satellite Io, it is the second most dense satellite among those whose densities are known.</p>
+				<p>It is thought to have formed nearly 4.5 billion years ago, not long after Earth. Although there have been several hypotheses for its origin in the past, the current most widely accepted explanation is that the Moon formed from the debris left over after a giant impact between Earth and a Mars-sized body.</p>
+      </article>
+      <article id="rocket">
+        <h2>Rocket Man</h2>
+        <p>Space exploration is the ongoing discovery and exploration of celestial structures in outer space by means of continuously evolving and growing space technology. While the study of space is carried out mainly by astronomers with telescopes, the physical exploration of space is conducted both by unmanned robotic probes and human spaceflight.</p>
+				<p>While the observation of objects in space, known as astronomy, predates reliable recorded history, it was the development of large and relatively efficient rockets during the early 20th century that allowed physical space exploration to become a reality. Common rationales for exploring space include advancing scientific research, uniting different nations, ensuring the future survival of humanity and developing military and strategic advantages against other countries.</p>
+      </article>
+      <article id="saturn">
+        <h2>See the Planets</h2>
+        <p>Planets are generally divided into two main types: large low-density gas giants, and smaller rocky terrestrials. Under IAU definitions, there are eight planets in the Solar System. In order of increasing distance from the Sun, they are the four terrestrials, Mercury, Venus, Earth, and Mars, then the four gas giants, Jupiter, Saturn, Uranus, and Neptune. Six of the planets are orbited by one or more natural satellites.</p>
+				<p>The idea of planets has evolved over its history, from the divine wandering stars of antiquity to the earthly objects of the scientific age. The concept has expanded to include worlds not only in the Solar System, but in hundreds of other extrasolar systems. The ambiguities inherent in defining planets have led to much scientific controversy.</p>
+      </article>
+    </div>
+  </body>
+</html> -->
