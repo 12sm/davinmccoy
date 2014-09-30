c99b7dae9773ac2949776c39961db20e9a19b83f
Sarah <Sarah@12southmusic.com>
2014-09-30 16:20:20 -0500

    brand on mobile only

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -8,7 +8,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+        <a class="navbar-brand hidden-lg hidden-md hidden-sm visible-xs" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
       </div>
 
       <nav class="collapse navbar-collapse" role="navigation">
