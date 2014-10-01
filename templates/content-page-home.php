90392c6c297f139d966beeac7730e8571b0274ee
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-01 15:01:33 -0500

    getting bg size

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,21 +1,50 @@
 <div id="stars"></div>
 
 <div id="images">
-  <img id="img-top" src="wp-content/themes/davinmccoy/assets/img/home-bg-2.jpg">
-  <img id="img-middle" src="wp-content/themes/davinmccoy/assets/img/home-bg-3.jpg">
-  <img id="img-bottom" src="wp-content/themes/davinmccoy/assets/img/photos-bg.jpg">
+  <div id="img-about">
+  </div>
+  <div id="img-shows">
+  </div>
+  <div id="img-videos">
+  </div>
+  <div id="img-photos">
+  </div>
 </div>
 
 <div id="homecontent">
-  <article id="about">
+  <article id="news">
     <div class='row'>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
+        <h1><a class="earth" href="#about">About</a></h1>
+      </div>
+      <div class='col-xs-3'>
         <h1><a class="moon" href="#shows">Shows</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="rocket" href="#videos">Videos</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
+        <h1><a class="saturn" href="#photos">Photos</a></h1>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col-sm-12'>
+        <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
+      </div>
+    </div>
+  </article>
+  <article id="about">
+    <div class='row'>
+      <div class='col-xs-3'>
+        <h1><a class="space" href="#news">News</a></h1>
+      </div>
+      <div class='col-xs-3'>
+        <h1><a class="moon" href="#shows">Shows</a></h1>
+      </div>
+      <div class='col-xs-3'>
+        <h1><a class="rocket" href="#videos">Videos</a></h1>
+      </div>
+      <div class='col-xs-3'>
         <h1><a class="saturn" href="#photos">Photos</a></h1>
       </div>
     </div>
@@ -27,13 +56,16 @@
   </article>
   <article id="shows">
     <div class='row'>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
+        <h1><a class="space" href="#news">News</a></h1>
+      </div>
+      <div class='col-xs-3'>
         <h1><a class="earth" href="#about">About</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="rocket" href="#videos">Videos</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="saturn" href="#photos">Photos</a></h1>
       </div>
     </div>
@@ -45,13 +77,16 @@
   </article>
   <article id="videos">
     <div class='row'>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
+        <h1><a class="space" href="#news">News</a></h1>
+      </div>
+      <div class='col-xs-3'>
         <h1><a class="earth" href="#about">About</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="moon" href="#shows">Shows</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="saturn" href="#photos">Photos</a></h1>
       </div>
     </div>
@@ -63,13 +98,16 @@
   </article>
   <article id="photos">
     <div class='row'>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
+        <h1><a class="space" href="#news">News</a></h1>
+      </div>
+      <div class='col-xs-3'>
         <h1><a class="earth" href="#about">About</a></h1>
       </div>
-      <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="moon" href="#shows">Shows</a></h1>
       </div>
-        <div class='col-xs-4'>
+      <div class='col-xs-3'>
         <h1><a class="rocket" href="#videos">Videos</a></h1>
       </div>
     </div>
