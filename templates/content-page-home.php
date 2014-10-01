c69b10147ffd5fa83b291171fbbfd50aee8be75f
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-01 16:56:01 -0500

    huieno

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -14,40 +14,12 @@
 <div id="homecontent">
   <article id="news">
     <div class='row'>
-      <div class='col-xs-3'>
-        <h1><a class="earth" href="#about">About</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="moon" href="#shows">Shows</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="rocket" href="#videos">Videos</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="saturn" href="#photos">Photos</a></h1>
-      </div>
-    </div>
-    <div class='row'>
       <div class='col-sm-12'>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
       </div>
     </div>
   </article>
   <article id="about">
-    <div class='row'>
-      <div class='col-xs-3'>
-        <h1><a class="space" href="#news">News</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="moon" href="#shows">Shows</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="rocket" href="#videos">Videos</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="saturn" href="#photos">Photos</a></h1>
-      </div>
-    </div>
     <div class='row'>
       <div class='col-sm-12'>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
@@ -56,20 +28,6 @@
   </article>
   <article id="shows">
     <div class='row'>
-      <div class='col-xs-3'>
-        <h1><a class="space" href="#news">News</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="earth" href="#about">About</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="rocket" href="#videos">Videos</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="saturn" href="#photos">Photos</a></h1>
-      </div>
-    </div>
-    <div class='row'>
       <div class='col-sm-12'>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
       </div>
@@ -77,40 +35,12 @@
   </article>
   <article id="videos">
     <div class='row'>
-      <div class='col-xs-3'>
-        <h1><a class="space" href="#news">News</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="earth" href="#about">About</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="moon" href="#shows">Shows</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="saturn" href="#photos">Photos</a></h1>
-      </div>
-    </div>
-    <div class='row'>
       <div class='col-sm-12'>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
       </div>
     </div>
   </article>
   <article id="photos">
-    <div class='row'>
-      <div class='col-xs-3'>
-        <h1><a class="space" href="#news">News</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="earth" href="#about">About</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="moon" href="#shows">Shows</a></h1>
-      </div>
-      <div class='col-xs-3'>
-        <h1><a class="rocket" href="#videos">Videos</a></h1>
-      </div>
-    </div>
     <div class='row'>
       <div class='col-sm-12'>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
