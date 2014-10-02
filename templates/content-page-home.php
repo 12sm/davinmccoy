44a4ec0a511108b9ad1a122f5033b511a72e2a2e
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-01 19:19:44 -0500

    ouityg

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -13,22 +13,45 @@
 
 <div id="homecontent">
   <article id="news">
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1>News</h1>
+      </div>
+    </div>
     <div class='row'>
       <div class='col-sm-12'>
-        <h1><a href="/news/">Read More News</a></h1>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
+      </div>
+    </div>
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1><a href="/news/">Read More News</a></h1>
       </div>
     </div>
   </article>
   <article id="about">
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1>About Davin</h1>
+      </div>
+    </div>
     <div class='row'>
       <div class='col-sm-12'>
-        <h1><a href="/about/">Read More About Davin</a></h1>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
+      </div>
+    </div>
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1><a href="/about/">Read More About Davin</a></h1>
       </div>
     </div>
   </article>
   <article id="shows">
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1>Shows</h1>
+      </div>
+    </div>
     <div class='row'>
       <div class='col-sm-12'>
         <div id="homedates">
@@ -43,18 +66,36 @@
     </div>
   </article>
   <article id="videos">
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1>Videos</h1>
+      </div>
+    </div>
     <div class='row'>
       <div class='col-sm-12'>
-        <h1><a href="/videos/">See More Videos</a></h1>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
+      </div>
+    </div>
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1><a href="/videos/">Watch More Videos</a></h1>
       </div>
     </div>
   </article>
   <article id="photos">
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1>Photos</h1>
+      </div>
+    </div>
     <div class='row'>
       <div class='col-sm-12'>
-        <h1><a href="/photos/">See More Photos</a></h1>
         <img src='/wp-content/themes/davinmccoy/assets/img/logo.png' class="img-responsive logo">
+      </div>
+    </div>
+    <div class="row">
+      <div class='col-sm-12'>
+        <h1><a href="/photos/">View More Photos</a></h1>
       </div>
     </div>
   </article>
