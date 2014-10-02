81a54f053a2d5b8a51a83cc566c4292428adf1e7
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-01 21:43:44 -0500

    rthiu

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -19,6 +19,7 @@
             // $("#content").css('top', (0 - scrollPosition) + 'px');
           }
           function doClick(x) {
+            debugger;
             if (window.location.href == "http://davinmccoy.12southdev.com")
               window.location.href = "http://davinmccoy.12southdev.com";
               $('html, body').animate({scrollTop : $(x).offset().top}, 1000, function(){
