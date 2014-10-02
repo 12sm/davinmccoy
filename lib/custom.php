bd05e9bc2e2fa4380fd80b08cddb874c2a34ad85
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-02 12:31:03 -0500

    manual excerpt

--- lib/custom.php
+++ lib/custom.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
 
 define( 'UPLOADS', ''.'assets' );
+
+//Manual excerpt scripts
+add_post_type_support('page','excerpt');
+
+//[about] shortcode
+function about_page () {
+  $page_id = 16;  //Page ID
+  $page_data = get_page( $page_id );
+  global $more;
+  $more = 0;
+  $content = apply_filters('the_content', $page_data->post_excerpt);
+  echo $content;
+}
+
+add_shortcode( 'about', 'about_page' );
 
 ?>
