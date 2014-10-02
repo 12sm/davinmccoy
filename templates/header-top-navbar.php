be0f18aab691a2cec6d87374e042a93d7f1935c2
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-01 22:16:18 -0500

    go

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -12,9 +12,9 @@
       </div>
       <nav id="main-nav" class="collapse navbar-collapse" role="navigation">
         <script>
-          var regex = /[#a-z]+$/g;
+          var regex = /^#[a-z]+$/g;
           function doClick(x) {
-            if (window.location.href != "http://davinmccoy.12southdev.com" && window.location.href != "http://davinmccoy.12southdev.com" + regex)
+            if (window.location.href != "http://davinmccoy.12southdev.com" && window.location.href != "http://davinmccoy.12southdev.com/" + regex)
               window.location.href = "http://davinmccoy.12southdev.com" + x;
           }
         </script>
