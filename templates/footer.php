506d896a589526ed804d55580c6ce67357f29edb
Sarah <Sarah@12southmusic.com>
2014-10-03 14:20:57 -0500

    rows and cols

--- templates/footer.php
+++ templates/footer.php
@@ -14,25 +14,35 @@
       	<?php // dynamic_sidebar('sidebar-footer'); ?>
       </div> -->
 	  <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="//davinmccoy.us8.list-manage.com/subscribe/post?u=4d33b75ac40b7c4a1aa594342&amp;id=7fb168a173" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe to the mailing list</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;"><input type="text" name="b_4d33b75ac40b7c4a1aa594342_7fb168a173" tabindex="-1" value=""></div>
-    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-    </div>
-</form>
-</div>
-
-<!--End mc_embed_signup-->
+	  
+			<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+			<style type="text/css">
+				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+				/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+				   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+			</style>
+				<div id="mc_embed_signup">
+				<form action="//davinmccoy.us8.list-manage.com/subscribe/post?u=4d33b75ac40b7c4a1aa594342&amp;id=7fb168a173" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+					<div class="row">
+						<div class="col-sm-4">
+							<label for="mce-EMAIL">Subscribe to the mailing list</label>
+						</div>
+						<div class="col-sm-4">
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						</div>
+						<div class="col-sm-4">
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;"><input type="text" name="b_4d33b75ac40b7c4a1aa594342_7fb168a173" tabindex="-1" value=""></div>
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+						</div>
+					</div><!-- end row -->
+					</div>
+				</form>
+				</div>
+			
+			
+			<!--End mc_embed_signup-->
 	</div>
 	<div class="col-sm-3">
       <!-- <div id="social" class="social-nav">
