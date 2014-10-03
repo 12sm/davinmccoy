e151935a44e6bf7299148288ee54051433071819
Sarah <Sarah@12southmusic.com>
2014-10-03 12:23:53 -0500

    footer this

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,17 @@
 <footer class="content-info container-fluid" role="contentinfo">
   <div class="row">
   	<div class="col-sm-4">
-      <div class="sidebar-footer">
-      	<?php dynamic_sidebar('sidebar-footer'); ?>
-      </div>
-	</div>
-	<div class="col-sm-4">
       <div class="footer-nav">
 			<?php
 		 if (has_nav_menu('footer_navigation')) :
 		   wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
 		 endif;
-	  ?>
+	        ?>
+      </div>
+	</div>
+	<div class="col-sm-4">
+      <div class="sidebar-footer">
+      	<?php dynamic_sidebar('sidebar-footer'); ?>
       </div>
 	</div>
 	<div class="col-sm-4">
@@ -22,8 +22,10 @@
 			// endif;
 		  ?>
       </div> -->
+	  <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
 	 </div>
-      <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
+	 
+      
 	
   </div><!--end row -->
 </footer>
