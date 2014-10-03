70ac8b2a7cc08952257ec6ac294ba8ef211d86f5
Sarah <Sarah@12southmusic.com>
2014-10-03 17:58:47 -0500

    get image

--- templates/content-single.php
+++ templates/content-single.php
@@ -4,6 +4,8 @@
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
+	<?php the_post_thumbnail('large', array('class' => 'alignleft')); ?>
+
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
