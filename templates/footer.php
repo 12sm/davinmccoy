5fe4557d7fc85f62369a876c6240900c994f43e0
Sarah <Sarah@12southmusic.com>
2014-10-06 13:09:23 -0500

    aligning responsively

--- templates/footer.php
+++ templates/footer.php
@@ -31,7 +31,7 @@
               <div class="col-sm-4">
                 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
               </div>
-              <div class="col-sm-4 text-center">
+              <div class="col-sm-4 subscribe-column">
                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                 <div style="position: absolute; left: -5000px;">
                   <input type="text" name="b_4d33b75ac40b7c4a1aa594342_7fb168a173" tabindex="-1" value="">
