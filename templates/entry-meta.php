df5932dc3489909484da5aa857d6057f6cb817e5
michael <michael@12southmusic.com>
2014-10-06 12:47:25 -0500

    Correct title and date for share

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,4 +1,4 @@
 <span class="share">
-	<div class="addthis_sharing_toolbox" data-title="[wpv-post-title]" data-url="[wpv-post-url]"></div>
+	<div class="addthis_sharing_toolbox"></div>
 </span>
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
