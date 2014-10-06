d1b78cb63102561908df5841732d9768619b3603
michael <michael@12southmusic.com>
2014-10-06 18:34:00 -0500

    Swapping out nav

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -11,49 +11,12 @@
         <a class="navbar-brand hidden-lg hidden-md hidden-sm visible-xs" href="<?php echo home_url(); ?>/"><img src="/wp-content/themes/davinmccoy/assets/img/dm-logo.png" height="30px" /></a>
       </div>
       <nav id="main-nav" class="collapse navbar-collapse" role="navigation">
-        <script>
-          function doClick(x, y) {
-            if (window.location.href != "http://davinmccoy.12southdev.com/")
-              window.location.href = y;
-          }
-        </script>
 
-<!-------- NAVBAR START -------->
-
-        <ul id="menu-primary-navigation" class="nav navbar-nav">
-          <li class="active menu-home">
-            <a class="space" onclick="doClick('#home', '/'); return false;">Home</a>
-          </li>
-          <li class="menu-about">
-            <a class="earth" href="/about">About</a>
-          </li>
-          <li class="menu-shows">
-            <a class="moon" onclick="doClick('#shows', '/shows/'); return false;">Shows</a>
-          </li>
-          <li class="menu-videos">
-            <a class="rocket" onclick="doClick('#videos', '/videos/'); return false;">Videos</a>
-          </li>
-          <li class="menu-photos">
-            <a class="saturn" onclick="doClick('#photos', '/photos/'); return false;">Photos</a>
-          </li>
-          <li class="menu-news">
-            <a href="/news/">News</a>
-          </li>
-          <li class="menu-store">
-            <a href="http://davinmccoy.myshopify.com/">Store</a>
-          </li>
-          <li class="menu-contact">
-            <a href="/contact/">Contact</a>
-          </li>
-        </ul>
-
-<!-------- NAVBAR END -------->
-
-        <!-- <?php
+        <<?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
           endif;
-        ?> -->
+          ?>
       </nav>
     </div>
   </div>
